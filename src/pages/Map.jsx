197d60908352b43746c908065196f083833a06e9
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-03-21 10:54:22 -0600

    map is up and running
    by:lincoln

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps';
+import googleAPIKey from "../hidden.js";
 
-function Map() {
+function Maps() {
+  const startingPosition = { lat: 37.29 , lng: -112.99 };
+  const [isMapInitialized, setMapInitialized] = useState(false);
   return (
-    <div className="map-page">Map</div>
+    <APIProvider apiKey={googleAPIKey} >
+      <div className="google-map" style={ {height: '100vh'} }>
+        <Map
+          zoom={isMapInitialized ? undefined : 15}
+          center={isMapInitialized ? undefined : startingPosition} 
+          onIdle={() => setMapInitialized(true)}
+        >
+        </Map>
+      </div>
+    </APIProvider>
   )
 }
 
-export default Map
+export default Maps
