7a8f820c2e0c7f5325b6bc3b58bfe3b93701834e
Caitlin Baldini <cat@listoglobal.com>
2024-03-27 11:56:47 -0600

    Merging main after adjustment

--- backend/server/controller.js
+++ backend/server/controller.js
@@ -360,11 +360,12 @@ const handlerFunctions = {
     const { username, password, bio, userPic } = req.body;
     console.log(req.body);
   },
+
   userInfo: async (req, res) => {
-    const { userId } = req.body;
-    console.log("Recieved userId:", userId);
+    // const { userId } = req.body
+    console.log("Recieved userId:", req.body.id);
     try {
-      const user = await User.findByPk(userId.userId, {
+      const user = await User.findByPk(req.body.id, {
         attributes: ["userId", "username", "password", "bio", "userPic"],
       });
       console.log("Retrieved user:", user);
@@ -375,6 +376,24 @@ const handlerFunctions = {
       res.status(500).send("Internal Server Error");
     }
   },
+  updateUser: async (req, res) => {
+    const { username, password, bio, userPic } = req.body;
+    console.log(req.body);
+  },
+  // userInfo: async (req, res) => {
+  //   const { userId } = req.body;
+  //   console.log("Recieved userId:", userId);
+  //   try {
+  //     const user = await User.findByPk(userId.userId, {
+  //       attributes: ["userId", "username", "password", "bio", "userPic"],
+  //     });
+  //     console.log("Retrieved user:", user);
+  //     res.send(user);
+  //   } catch (error) {
+  //     console.error("Error retrieving user:", error);
+  //     res.status(500).send("Internal Server Error");
+  //   }
+  // },
   updateUser: async (req, res) => {
     const { username, password, bio, userPic } = req.body;
     console.log(req.body);
